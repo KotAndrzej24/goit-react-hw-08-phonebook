ee2d6b4e0d753b80e043bdc367fbae0f78f2e4ec
Qube24 <k.twerdochlib@gmail.com>
2023-06-19 15:45:42 +0200

    little fixes

--- src/index.js
+++ src/index.js
@@ -31,7 +31,7 @@ const theme = createTheme({
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/goit-react-hw-08-phonebook">
+    <BrowserRouter basename="/Phonebook-in-React">
       <PersistGate loading={null} persistor={persistor}>
         <Provider store={store}>
           <ThemeProvider theme={theme}>
